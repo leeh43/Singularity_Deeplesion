ca15abd69759181b5a7637a70b77f06f1d40e2ee
leeh43 <leeh43@masi-32.ds.vanderbilt.edu>
2020-04-12 23:33:29 -0500

    Change setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ virtualenv venv --python=python3.6
 source venv/bin/activate
 
 echo "installing pytorch 1.1. You may need to go to https://pytorch.org/ to find the best way to install on your machine (OS, CUDA etc.)"
-pip install torch==1.1.0 -f https://download.pytorch.org/whl/cu100/stable
-pip install torchvision==0.3.0
-pip install --upgrade cython
+pip3 install torch==1.1.0 -f https://download.pytorch.org/whl/cu100/stable
+pip3 install torchvision==0.3.0
+pip3 install --upgrade cython
 
 # necessary python libraries
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 sudo apt install python3-tk
 
